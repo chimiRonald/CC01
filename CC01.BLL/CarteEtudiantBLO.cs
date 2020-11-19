9ad958bdeec8b8261b94315dbe45c0c0db4cf8e0
genotypecode <67495031+GeneotypeCode@users.noreply.github.com>
2020-11-19 16:23:32 +0100

    BLL OKAY

---
+++ CC01.BLL/CarteEtudiantBLO.cs
@@ -0,0 +1,37 @@
+﻿using CC01.BO;
+using CC01.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CC01.BLL
+{
+   public  class CarteEtudiantBLO
+    {
+        CarteEtudiantDAO carteEtudiantRepo;
+        public  CarteEtudiantBLO(string dbFolder)
+        {
+            carteEtudiantRepo = new CarteEtudiantDAO(dbFolder);
+            
+        }
+        public void CreateCarteEtudiant(CarteEtudiant  carteEtudiant)
+        {
+            carteEtudiantRepo.Add(carteEtudiant);
+
+        }
+        public void DeleteCarteEtudiant (CarteEtudiant carteEtudiant)
+        {
+            carteEtudiantRepo.Remove(carteEtudiant);
+        }
+        public IEnumerable<CarteEtudiant>GetCarteEtudiants()
+        {
+            return carteEtudiantRepo.Find();
+        }
+        public IEnumerable<CarteEtudiant> GetBy(Func<CarteEtudiant,bool>Predicate)
+        {
+            return carteEtudiantRepo.Find(Predicate);
+        }
+    }
+}
